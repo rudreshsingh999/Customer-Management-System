84b765f1076efa83bcb75d47a41f3906b20ddb90
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2020-07-02 01:41:29 +0530

    Update routes to add new pages

--- public/src/App.js
+++ public/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login'
 import SignUp from './SignUp'
+import Payment from './Payment'
+import Homepage from './Homepage'
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/dashboard' component={Dashboard} />
+                <Route exact path='/dashboard' component={Homepage} />
                     <Route exact path='/' component={Login} />
                     <Route exact path='/signup' component={SignUp} />
+                    <Route exact path='/customerPanel' component={Dashboard} />
+                    <Route exact path='/payment' component={Payment} />
                 </Switch>
             </BrowserRouter>
         );
